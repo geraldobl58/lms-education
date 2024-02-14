1d5a814da3b4a3a2136857e0136fb57a21641d65
Geraldo Luiz <geraldobl58@gmail.com>
2024-02-14 18:41:17 -0300

    feat: Add layout dashboard page

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,18 @@
+import { Sidebar } from "./_components/sidebar";
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+        <Sidebar />
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default DashboardLayout;
