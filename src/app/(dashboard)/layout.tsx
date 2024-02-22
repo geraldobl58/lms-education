0dbbe931a98ec7c4ebebabb3b91fc783599fd7f6
Geraldo Luiz <geraldobl58@gmail.com>
2024-02-22 18:50:28 -0300

    feat: Add navbar layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from "./_components/navbar";
 import { Sidebar } from "./_components/sidebar";
 
 interface DashboardLayoutProps {
@@ -8,12 +9,12 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <div className="h-full">
       <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
-        Navbar
+        <Navbar />
       </div>
       <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
         <Sidebar />
       </div>
-      <main className="md:pl-56 h-full">{children}</main>
+      <main className="md:pl-56 pt-[80px] h-full">{children}</main>
     </div>
   );
 };
