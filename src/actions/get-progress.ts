2a8827bc30ca734cfd8369ac21f4567e9257c94a
Geraldo Luiz <geraldobl58@gmail.com>
2024-02-27 15:43:25 -0300

    feat: Add get progress helper

---
+++ src/actions/get-progress.ts
@@ -0,0 +1,38 @@
+import { db } from "@/lib/db";
+
+export const getProgress = async (
+  userId: string,
+  courseId: string
+): Promise<number> => {
+  try {
+    const publishedChapters = await db.chapter.findMany({
+      where: {
+        courseId: courseId,
+        isPublished: true,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    const publishedChaptersIds = publishedChapters.map((chapter) => chapter.id);
+
+    const validateCompletedChapters = await db.userProgress.count({
+      where: {
+        userId: userId,
+        chapterId: {
+          in: publishedChaptersIds,
+        },
+        isCompleted: true,
+      },
+    });
+
+    const progressPercentage =
+      (validateCompletedChapters / publishedChapters.length) * 100;
+
+    return progressPercentage;
+  } catch (error) {
+    console.log(error);
+    return 0;
+  }
+};
