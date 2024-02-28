3cdabbb79ab93d40f1fe969181caecc6720b70f9
Geraldo Luiz <geraldobl58@gmail.com>
2024-02-28 18:53:59 -0300

    feat: Add layout dashboard

--- src/app/(dashboard)/(routes)/page.tsx
+++ src/app/(dashboard)/(routes)/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div>
       <div>Hello</div>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
